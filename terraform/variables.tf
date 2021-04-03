7b16a639ac6afca7e6658d8684c09b0d05a789a7
EJ Best <erich.ej.best@gmail.com>
2021-04-03 07:32:46 -0400

    updated to match Vault

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,15 @@
 variable "aws_region" {
   default = "us-east-1"
 }
-variable "access_key" {
-  default=""
-}
-variable "secret_key" {
-  default = ""
-}
-variable "vault_addr" {
-  default=""
-}
-variable "vault_token" {
-  default = ""
-}
+
 variable "cluster-name" {
   default = "terraform-eks-demo"
   type    = string
+}
+
+variable "vault_addr" {
+  default="https://vault-cluster.vault.5ed9cf1f-8efb-40c3-b197-36720dc42a94.aws.hashicorp.cloud:8200"
+}
+variable "vault_token" {
+  default = "s.rgncokPdWViaVMfPf5ggQgTh.XTbML"
 }
