a503eaeb0893aa193f1defb748655d4489f97535
EJ Best <erich.ej.best@gmail.com>
2021-04-04 08:48:20 -0400

    updated with changed 4/3/21

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -14,10 +14,9 @@ data "vault_aws_access_credentials" "creds" {
 
 ### fetches aws access and secert key form vault
 provider "aws" {
-  region	 = "${var.aws_region}"
-  access_key = "${data.vault_aws_access_credentials.creds.access_key}"
-  secret_key = "${data.vault_aws_access_credentials.creds.secret_key}"
-  
+  region	 = var.aws_region
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key  
 }
 
 resource "aws_iam_role" "demo-cluster" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,9 +7,12 @@ variable "cluster-name" {
   type    = string
 }
 
+
+
 variable "vault_addr" {
   default="https://vault-cluster.vault.5ed9cf1f-8efb-40c3-b197-36720dc42a94.aws.hashicorp.cloud:8200"
 }
+
 variable "vault_token" {
-  default = "s.rgncokPdWViaVMfPf5ggQgTh.XTbML"
+  default = "s.PYx3djggEfrzeyC34y0A2Cfc.XTbML"
 }
