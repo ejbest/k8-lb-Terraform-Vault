743421009a52e938463d5a6dd0ee6d246ba5ab2a
Erich (EJ) Best <20603184+ejbest@users.noreply.github.com>
2024-10-28 16:49:56 -0400

    clean up

--- zold/application/rebuild-k8-app.sh
+++
@@ -1,8 +0,0 @@
-#
-# this will build the docker contianer image
-# the container will be pushed to ejbest docker hub as public
-cd apline 
-docker build . -t ejbest/alpine 
-docker push ejbest/alpine 
-cd ..
-
